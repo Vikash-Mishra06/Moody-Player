4b3a15712bdf31b4202a374e62b57499c61de448
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-03 23:47:10 +0530

    `Update package-lock.json and package.json to include new dependencies and remove old ones`

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDb = require("./src/db/db");
 
 connectDb();
 app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
+const songRoutes = require("./routes/song.routes")
 
+app.use(express.json());
+
+app.use('/', songRoutes)
 module.exports = app;
