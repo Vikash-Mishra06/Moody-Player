17a8fdde4a6af2b0d0ba5333f939eb04416806a6
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-03 20:07:30 +0530

    Backend Integration

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("./src/app");
+const express = require("express");
+const connectDb = require("./src/db/db");
+
+connectDb();
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
